a1ecf578a3eb0a78026f7cbe566dfc8acc6c4eef
ujdcodr <ujjwalpasupulety@gmail.com>
2018-03-06 14:26:06 +0530

    Added Student and Faculty pages

--- code.py
+++ code.py
@@ -1,5 +1,42 @@
 from tkinter import *
 
+class StuGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Course Management System")
+
+        self.label = Label(master, text="Student Page")
+        self.label.pack()
+        
+        self.but1 = Button(master, text="B1", command=self.B1)
+        self.but1.pack(side="bottom")
+        '''
+        self.close_button = Button(master, text="Logout", command=master.quit)
+        self.close_button.pack(side="bottom")
+        '''        
+        
+    def B1(self):
+        print("Hello")
+
+class InsGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Course Management System")
+
+        self.label = Label(master, text="Instructor Page")
+        self.label.pack()
+        
+        self.but1 = Button(master, text="B1", command=self.B1)
+        self.but1.pack(side="bottom")
+        '''
+        self.close_button = Button(master, text="Logout", command=master.quit)
+        self.close_button.pack(side="bottom")
+        '''        
+        
+    def B1(self):
+        print("Hello")
+
+        
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
@@ -18,10 +55,10 @@ class MyFirstGUI:
         self.passw = Entry(master,show="*")
         self.passw.pack(side="left")
 
-
+        '''  
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack(side="bottom")
-        
+        self.close_button.pack(side="bottom") 
+        '''        
         self.v = IntVar()
         self.stud =Radiobutton(master,text="Student", variable=self.v,value=1).pack(side="bottom")
         self.ins =Radiobutton(master,text="Instructor", variable=self.v,value=2).pack(side="bottom")
@@ -33,9 +70,19 @@ class MyFirstGUI:
         print(self.name.get())
         print(self.passw.get())
         print(self.v.get())
-        
-        print("Logged In")
-
+        if(self.v.get()==1):
+            print("Student Logged In")
+            stud = Tk()
+            stud.minsize(200,200)
+            stu_gui = StuGUI(stud)
+            stud.mainloop()
+        if(self.v.get()==2):
+            print("Instructor Logged In")
+            ins = Tk()
+            ins.minsize(200,200)
+            ins_gui = InsGUI(ins)
+            ins.mainloop()
+         
 root = Tk()
 root.minsize(200,200)
 my_gui = MyFirstGUI(root)
